456d4a3e37e2d4e36ec4260aeb40f6406a501266
imrojhimal <e193021@ugrad.iiuc.ac.bd>
2025-11-08 22:29:27 +0600

    vector linear search

--- Datastructure/veclinear.cpp
+++ Datastructure/veclinear.cpp
@@ -3,4 +3,25 @@ using namespace std;
 int main()
 {
     vector<int>vec;
+    int size;
+    cout<<"you wanna fix the size :";
+    cin>>size;
+    cout<<endl;
+    int input;
+    for(int i=0;i<size;i++)
+    {
+          cin>>input;
+          vec.push_back(input);
+    }
+    cout<<"what do you wanna search : ";
+    int target,count=0;
+    cin>>target;
+    for(int val:vec)
+    {
+        if(val==target)
+        {
+            cout<<endl<<"found at index :"<<count<<endl<<"element is :"<<val;
+        }
+        count++;
+    }
 }
