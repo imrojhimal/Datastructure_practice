ced01bf86d18bc818e4aaa3461922e2b3cfe4572
imrojhimal <e193021@ugrad.iiuc.ac.bd>
2025-11-08 23:09:13 +0600

    using two pointer approach and reference function for reversing

---
+++ Datastructure/vectorreverse.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+void vecreverse(vector<int>&v)
+{
+    int start;
+    int end;
+    start=0;
+     end=v.size()-1;
+      while(start<end)
+    {
+        swap(v[start],v[end]);
+        start++;
+        end--;
+       
+    }
+    for(int val :v)
+    {
+        cout<<val<<" ";
+    }
+}
+int main()
+{
+    vector<int>vec={55,56,65,78,89,65,69};
+    cout<<"Vector before reverse :";
+    for(int val :vec)
+    {
+        cout<<val<<" ";
+    }
+cout<<endl<<"vector after reverse :";
+vecreverse(vec);   
+}
