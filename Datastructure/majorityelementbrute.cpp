54b29460f6771d03f039880bb3e1003aea56b58e
Imroj Ahasan Himal <imrojhimal@gmail.com>
2025-12-20 13:08:49 +0600

    majority element brute force approach

---
+++ Datastructure/majorityelementbrute.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int majority(vector<int>&nums)
+{
+    int n=nums.size();
+    int freq;
+    for(int val:nums)
+    {
+        freq=0;
+        for(int mj:nums)
+        {
+            if(val==mj)
+            {
+                freq++;
+            }
+        }
+        if(freq>n/2)
+        {
+            return val;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    vector<int> nums={2,2,1,1,1,2,2};
+    cout<<"the majority element is  "<<majority(nums);
+}
